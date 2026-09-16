102e12d6b1f424e2eb4db7ee2b7efd8a2f94aef5
Md Sijan Mahmud <sijanmahmud2007@gmail.com>
2026-09-17 02:25:59 +0600

    Update ALLOWED_HOSTS for Render

--- campus_complaint/settings.py
+++ campus_complaint/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%7+w_2(1yr!07t803*ghfsuv1!_*ht@=n3p$!$pvoazsprhfv^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
