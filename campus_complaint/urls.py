dfc66c7c2a48ba91cae5bdae449368e6703e3b58
Md Sijan Mahmud <sijanmahmud2007@gmail.com>
2026-09-17 09:03:10 +0600

    Fix media files URL for production

--- campus_complaint/urls.py
+++ campus_complaint/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# এখানে 'if settings.DEBUG:' চেকটি সরিয়ে দেওয়া হয়েছে, যেন রেন্ডারে লাইভ প্রজেক্টেও ছবিগুলো দেখা যায়
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
